0ab31f2d212aa26cafd363bc50fae678d850b0e4
SamanthaSmith04 <Troyandme04@gmail.com>
2025-10-31 10:09:39 -0400

    Basic pulse server setup

--- pulse_support/pulse_support/pulse_server.py
+++ pulse_support/pulse_support/pulse_server.py
@@ -17,6 +17,25 @@ class PulseServer(Node):
 
     def pulse_callback(self, request: Pulse.Request, response: Pulse.Response) -> Pulse.Response:
         self.get_logger().info(f"Received pulse request: {request}")
+
+        params = pulse.PulseParams(
+            sigma_u = request.pulse_params.sigma_u,
+            sigma_v = request.pulse_params.sigma_v,
+            a = request.pulse_params.a,
+            ref_dist = request.pulse_params.ref_dist,
+            samples_per_pulse = request.pulse_params.samples_per_pulse
+        )
+
+        p = pulse.Pulse(params)
+        p.load_mesh(request.mesh_filepath)
+        res = p.evaluate_poses(request.poses)
+        res_scaled = p.get_scaled_thicknesses(res)
+        
+        response.texture.height = res_scaled.shape[0]
+        response.texture.width = res_scaled.shape[1]
+        response.texture.data = (res_scaled.flatten() * 255).astype('uint8').tolist()
+        response.message = "Pulse operation completed successfully."
+
         response.success = True
         return response
 
